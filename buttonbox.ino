ea67220f9e5eaa5c9b890e71bffb1155cc28bddd
Jacendb <32490960+Jacendb@users.noreply.github.com>
2021-03-18 21:05:53 -0400

    Initial commit
    Saving my work
    * Switch connected to pins 0 and 1
    * Push button connected to pin A0

---
+++ buttonbox.ino
@@ -0,0 +1,70 @@
+#include <Joystick.h>
+
+// Create the Joystick
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
+  3, 0,                  // Button Count, Hat Switch Count
+  false, false, false,     // X and Y, but no Z Axis
+  false, false, false,   // No Rx, Ry, or Rz
+  false, false,          // No rudder or throttle
+  false, false, false);  // No accelerator, brake, or steering
+;
+
+// Constant that maps the physical pin to the joystick button.
+const int pinTotoggle1Map = 0;
+const int pinTotoggle2Map = 1;
+const int pinToButtonMap = 2;
+
+void setup() {
+//  Serial.begin(9600);
+
+  // Initialize Button Pins
+  pinMode(0, INPUT_PULLUP );
+  pinMode(1, INPUT_PULLUP );
+  pinMode(A0, INPUT_PULLUP );
+
+  // Initialize Joystick Library
+  Joystick.begin();
+}
+
+// Last state of the button
+int lastButtonState[3] = {0,0,0};
+
+void loop() {
+
+  // Read pin values for switch
+  for (int index = 0; index < 2; index++)
+  {
+    int currentButtonState = !digitalRead(index);
+    if (currentButtonState != lastButtonState[index])
+    {
+      Joystick.setButton(index, currentButtonState);
+      lastButtonState[index] = currentButtonState;
+    }
+  }
+
+  // Read pin values for button
+//  for (int index = 2; index < 3; index++)
+//  {
+//    int currentButtonState = digitalRead(index);
+//    if (currentButtonState != lastButtonState[index])
+//    {
+//      Joystick.setButton(index, currentButtonState);
+//      lastButtonState[index] = currentButtonState;
+//    }
+//  }
+
+//  if (digitalRead(A0) != 0)
+//  {
+      int currentButtonState = digitalRead(A0);
+      if (currentButtonState != lastButtonState[2])
+      {
+        Joystick.setButton(2, currentButtonState);
+        lastButtonState[2] = currentButtonState;
+      }
+//  }
+  //  Serial.print("\ncurrent button state:");
+  //  Serial.print(currentButtonState);
+  //  Serial.print("\nlast button state:");
+  //  Serial.print(lastButtonState);
+  delay(10);
+}
