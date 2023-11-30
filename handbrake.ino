b0bf0d29e2ef06607d3f9ce5401a747120446cf1
Jacendb <32490960+Jacendb@users.noreply.github.com>
2023-11-29 19:46:36 -0500

    Update handbrake.ino

--- handbrake.ino
+++ handbrake.ino
@@ -8,15 +8,16 @@
 #include <Joystick.h>
 
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
-                   JOYSTICK_TYPE_MULTI_AXIS, 0, 0,
+                   JOYSTICK_TYPE_JOYSTICK, 0, 0,
                    false, false, false, false, false, false,
                    true, false, false, false, false);
 
 void setup()
 {
-//  Serial.begin(9600);
+  //Serial.begin(9600);
   pinMode(A2, INPUT);
   Joystick.begin();
+  Joystick.setRudderRange(0, 255);
 }
 
 // const int pinToButtonMap = A2;
@@ -28,18 +29,22 @@ void loop()
   int pot = analogRead(A2);
   int mapped;
   mapped = map(pot, 450, 650, 0, 255);
+  //Serial.println(mapped);
 
-  if(pot > 485)
+  if(mapped > 50)
   {
-    Joystick.setRudder(pot);
+    Joystick.setRudder(mapped);
   }
   else
   {
-    if (last != 482)
-    {
-      Joystick.setRudder(pot);
-    }
+    Joystick.setRudder(0);
+    // if (last != 255)
+    // {
+    //   Joystick.setRudder(mapped);
+    // }
   }
-  last = pot;
+  //Serial.print("Mapped:");
+  
+  //Serial.println(pot);
   delay(10);
 }
